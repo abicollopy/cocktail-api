f1812c3c6021ac488b6e303320942f93334bd2be
abicollopy <a.collopy@hotmail.co.uk>
2021-02-26 11:16:22 +0000

    random cocktails

--- src/navBar/RandomCocktail/index.tsx
+++ src/navBar/RandomCocktail/index.tsx
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const apiRandomEndpoint = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+
+const searchRandom = async () => {
+  const response = await fetch(apiRandomEndpoint);
+  const responseJson = await response.json();
+  return responseJson;
+};
+
+interface DrinkInfoModel {
+  strDrink: string;
+  strInstructions: string;
+  strDrinkThumb: string;
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
+  strIngredient4: string;
+  strIngredient5: string;
+  strIngredient6: string;
+  strIngredient7: string;
+  strIngredient8: string;
+  strIngredient9: string;
+  strIngredient10: string;
+  strIngredient11: string;
+  strIngredient12: string;
+  strIngredient13: string;
+  strIngredient14: string;
+  strIngredient15: string;
+  strMeasure1: string;
+  strMeasure2: string;
+  strMeasure3: string;
+  strMeasure4: string;
+  strMeasure5: string;
+  strMeasure6: string;
+  strMeasure7: string;
+  strMeasure8: string;
+  strMeasure9: string;
+  strMeasure10: string;
+  strMeasure11: string;
+  strMeasure12: string;
+  strMeasure13: string;
+  strMeasure14: string;
+  strMeasure15: string;
+}
 
 export default () => {
-  console.log('HI');
+  const [responseDrinkInfo, setResponseDrinkInfo] = useState<DrinkInfoModel>();
+  const {
+    strDrink = '', strInstructions = '', strDrinkThumb = '',
+  } = responseDrinkInfo || {};
+  useEffect(() => {
+    searchRandom().then((responseJson) => {
+      const { drinks: [drinkInfo] } = responseJson;
+      setResponseDrinkInfo(drinkInfo);
+    });
+  }, []);
   return (
-    <div>BEAUTIFUL RANDOM</div>
+    <div>
+      {strDrink}
+      <br />
+      {strInstructions}
+      <img src={strDrinkThumb} alt="" />
+    </div>
   );
 };
